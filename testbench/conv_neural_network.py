e933ce2b9bf3e9f4c76a06e90c42a0e90e269e21
Thodoris Evangelakos <tevangelakos@tuc.gr>
2023-12-17 17:17:59 +0200

    Added documentation for the testbench. Also made some functions prettier

--- testbench/conv_neural_network.py
+++ testbench/conv_neural_network.py
@@ -1,15 +1,20 @@
+"""
+This module contains a convolutional neural network model for image classification using PyTorch.
+
+The module includes the following classes and functions:
+- Net: A convolutional neural network model.
+- main: Train and evaluate the neural network.
+- load_data_for_model: Loads and prepares the CIFAR-10 dataset for training and testing.
+- quick_test: Performs a quick test of the convolutional neural network model.
+"""
+
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 from tqdm import tqdm
-
-
-#risky
-#import multiprocessing
-#multiprocessing.set_start_method('spawn', force=True)
 
 #ATTEMPTED CONSTANTS
 #   DATASET
@@ -24,6 +29,24 @@ POOL_STRIDE = 2
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Net(nn.Module):
+    """
+    A convolutional neural network model.
+
+    Args:
+        cl_num (int): The number of convolutional layers.
+        fc_num (int): The number of fully connected layers.
+        pl_num (int): The number of pooling layers.
+
+    Attributes:
+        conv_layers (nn.ModuleList): A list of convolutional layers.
+        pool (nn.MaxPool2d): The pooling layer.
+        fully_conn_layers (nn.ModuleList): A list of fully connected layers.
+
+    Methods:
+        _get_flattened_size: Calculates the size of the flattened features 
+            after conv and pool layers.
+        forward: Performs the forward pass of the network.
+    """
     def __init__(self, cl_num: int, fc_num: int, pl_num: int):
         super(Net, self).__init__()
 
@@ -33,7 +56,9 @@ class Net(nn.Module):
         for i in range(cl_num):
             #could make it prettier by using out = calc(i) and in = calc(i-1)
             output_channels = 32*(2**i)
-            self.conv_layers.append(nn.Conv2d(in_channels=in_channel_cond(i), out_channels=output_channels, kernel_size=KERNEL_SIZE, padding=PADDING))
+            self.conv_layers.append(nn.Conv2d(in_channels=in_channel_cond(i),
+                                              out_channels=output_channels,
+                                              kernel_size=KERNEL_SIZE, padding=PADDING))
 
         # Pooling layer
         self.pool = nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE)
@@ -59,6 +84,15 @@ class Net(nn.Module):
         return flattened_size
 
     def forward(self, x):
+        """
+        Performs forward pass through the convolutional neural network.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after passing through the network.
+        """
         #applying conv layers with pooling
         for conv_layer in self.conv_layers:
             x = self.pool(F.relu(conv_layer(x)))
@@ -70,7 +104,8 @@ class Net(nn.Module):
             x = F.relu(fc_layer(x)) if i < len(self.fully_conn_layers) - 1 else fc_layer(x)
         return x
 
-def main(cl_num, fc_num, pl_num, num_epochs, trainloader, testloader, optimizer_type='adam', learning_rate=0.001):
+def main(cl_num, fc_num, pl_num, num_epochs, trainloader,
+         testloader, optimizer_type='adam', learning_rate=0.001):
     """
     Train and evaluate the neural network.
 
@@ -136,15 +171,50 @@ def main(cl_num, fc_num, pl_num, num_epochs, trainloader, testloader, optimizer_
 
 
 def load_data_for_model(batch_size=4, num_workers=2):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    """
+    Loads and prepares the CIFAR-10 dataset for training and testing.
+
+    Args:
+        batch_size (int): The number of samples per batch. Default is 4.
+        num_workers (int): The number of worker threads for data loading. Default is 2.
+
+    Returns:
+        trainloader (torch.utils.data.DataLoader): DataLoader for training set.
+        testloader (torch.utils.data.DataLoader): DataLoader for test set.
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers
+    )
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=num_workers
+    )
     print("Data loaded successfully.")
     return trainloader, testloader
 
 def quick_test():
+    """
+    Function to perform a quick test of the convolutional neural network model.
+
+    This function loads the data for the model, defines test configurations,
+    and runs the main function for each configuration.
+    It prints the configuration and accuracy for each test.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     trainloader, testloader = load_data_for_model()
 
     # Test configurations
@@ -154,7 +224,8 @@ def quick_test():
     ]
 
     for config in test_configs:
-        accuracy = main(config['cl_num'], config['fc_num'], config['pl_num'], config['num_epochs'], trainloader, testloader)
+        accuracy = main(config['cl_num'], config['fc_num'], config['pl_num'],
+                        config['num_epochs'], trainloader, testloader)
         print(f"Config: {config}, Accuracy: {accuracy}%")
 
 if __name__ == "__main__":
--- testbench/small_wrapper.py
+++ testbench/small_wrapper.py
@@ -16,50 +16,66 @@ FCS = 2       # Fully Connected Layers
 PLS = 2       # Pooling Layers
 
 def load_data_for_model(batch_size=4, num_workers=2):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    """
+    Loads the CIFAR-10 dataset and creates data loaders for training and testing.
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    Args:
+        batch_size (int): The number of samples per batch. Default is 4.
+        num_workers (int): The number of worker threads for data loading. Default is 2.
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    Returns:
+        trainloader (torch.utils.data.DataLoader): Data loader for training set.
+        testloader (torch.utils.data.DataLoader): Data loader for testing set.
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
+    )
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
 
     print("Data loaded successfully.")
-    
+
     return trainloader, testloader
 
-'''
-def train_and_evaluate_model(trainloader, testloader, cl_num, fc_num, epochs):
-    net = Net(cl_num, fc_num, PLS)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
-
-    # Training
-    for epoch in range(epochs):
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-    # Evaluation
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    accuracy = 100 * correct / total
-    return accuracy
-'''
-
 def model_eval():
+    """
+    Evaluate the model's performance on different configurations.
+
+    This function loads the data for the model and iterates over different configurations
+    of convolutional layers and epochs. It calls the `cnn.main` function to train and test
+    the model with each configuration and prints the accuracy.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     trainloader, testloader = load_data_for_model()
 
     for cl in CLS:
@@ -68,13 +84,31 @@ def model_eval():
             print(f'Epochs: {epoch}, Convolutional Layers: {cl}, Accuracy: {accuracy}%')
 
 def gpu_test():
+    """
+    Function to test GPU availability and name.
+
+    Prints the GPU availability and name using torch.cuda.is_available()
+    and torch.cuda.get_device_name(0) respectively.
+    """
     print("GPU Test")
     print("GPU Available: ", torch.cuda.is_available())
     print("GPU Name: ", torch.cuda.get_device_name(0))
     print(20*"~")
 
 def menu():
-    print("What do you want to do?\n1   Evaluate model\n2   GPU Testt\n3   Exit")
+    """
+    Displays a menu and prompts the user for a choice.
+    
+    The function prints a menu with options to evaluate a model, perform a GPU test, or exit the program.
+    It then prompts the user to enter their choice and executes the corresponding action based on the input.
+    If an invalid choice is entered, it displays an error message and recursively calls itself to display the menu again.
+    """
+    print("========== MENU ==========")
+    print("What do you want to do?")
+    print("1. Evaluate model")
+    print("2. GPU Test")
+    print("3. Exit")
+    
     choice = input("Enter your choice: ")
     
     match choice:
@@ -87,7 +121,7 @@ def menu():
         case _:
             print("Invalid choice. Try again.")
             menu()
-    menu()
 
 if __name__ == "__main__":
     menu()
+
